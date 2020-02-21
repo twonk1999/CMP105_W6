b06046c38aeddaff34b581ec3a2b96250e17ec56
1901042-Arran_Donbavand <Arrandonbavand@yahoo.co.uk>
2020-02-21 09:04:36 +0000

    Made edits to Mario header file
    Made some edits to the Mario header file to allow for the addition of Gravity and other features

--- Week6/CMP105App/Mario.h
+++ Week6/CMP105App/Mario.h
@@ -1,7 +1,23 @@
 #pragma once
 #include "Framework/GameObject.h"
+#include "Framework/Animation.h"
+#include "Framework/Input.h"
 class Mario : public GameObject
 {
+public:
+	sf::Vector2f gravity = sf::Vector2f(0, 0);
+	sf::Vector2f velocity = sf::Vector2f(0, 0);
+	sf::Vector2f jumpVector = sf::Vector2f(0, 0);
+	sf::Vector2f pos = sf::Vector2f(0, 0);
 
+
+	float scale = 0;
+	float speed = 0;
+	bool isJumping = false;
+
+	Animation marioWalk;
+protected:
+	void update(float dt) override;
+	void handleInput(float dt);
 };
 
